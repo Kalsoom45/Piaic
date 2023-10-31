a06863a9e074f9fc1cf9ab440171325fb9a71902
Kalsoom45 <149462935+Kalsoom45@users.noreply.github.com>
2023-10-31 20:21:36 +0500

    Create Q11.ts

---
+++ Exercise/Q11.ts
@@ -0,0 +1,14 @@
+
+let names: string[]=['Habiba', 'Ali', 'Reena', 'Kalsoom];
+
+console.log(`1st name from list: ${names[0]}`);
+
+console.log(`2nd name from list: ${names[1]}`);
+
+console.log(`3rd name from list: ${names[2]}`);
+
+console.log(`4th name from list: ${names[3]}`);
+
+
+for (let i = 0; i < names.length; i++) 
+ { console.log(`\nWith for loop: At index ${i} name in list is : ${names[i]}`) }
